955f7199fa9653ef2f1b8bed71be3d266182b761
João <joaolucassilvasimoes123@gmail.com>
2026-07-08 18:39:16 -0300

    Add function jQuery,openSettings and closeSettings

---
+++ public/javaScript/menuConfig.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+
+
+    $(document).on('click', '#openSettings', function () {
+        window.location.href = '/public/html/menuConfig.html';
+    });
+
+
+    $(document).on('click', '#buttonClose', function () {
+        window.location.href = '/public/html/home.html';
+    });
+});
