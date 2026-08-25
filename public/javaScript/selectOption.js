724f4a639ca62f71d32277c87261a7f2b7ef8130
João <joaolucassilvasimoes123@gmail.com>
2026-08-25 18:25:59 -0300

    Concertando o selectOption.js

--- public/javaScript/selectOption.js
+++ public/javaScript/selectOption.js
@@ -1,28 +1,40 @@
 $(document).ready(function () {
 
-    const selectOptions = [];
+    function getStorageKey(card) {
+        return 'card-selecionado-' + card.data('streaming');
+    }
 
+    function restoreSelectedCards(container) {
+        $(container).find('.cfg-card[data-streaming]').each(function () {
+            var card = $(this);
+            var estaSelecionado = localStorage.getItem(getStorageKey(card));
 
-    $(document).on('click', '.cfg-card-icon', function () {
-
-        // Adiciona o elemento clicado ao vetor
-        selectOptions.push(this);
-
-
-        // Percorre os elementos selecionados
-        for (let i = 0; i < selectOptions.length; i++) {
-
-            // Pega o valor do elemento selecionado
-            let value = $(selectOptions[i]).attr('alt');
-
-
-            // Procura o card que possui esse mesmo value
-            $(`.cardSelect[value="${value}"]`).css({
-                'border': '3px solid green'
-            });
-
+        if (estaSelecionado === 'true') {
+                card.addClass('cfg-card-selecionado');
+            }
         }
+        );
+    }
 
+    // O menu é inserido dinamicamente na Home; restaura as escolhas após o carregamento.
+    $(document).on('settings:loaded', function (event, container) {
+        restoreSelectedCards(container);
+    });
+
+    // Clique simples: marca o card com borda verde e salva no localStorage
+    $(document).on('click', '.cfg-card', function () {
+        var card = $(this);
+
+        card.addClass('cfg-card-selecionado');
+        localStorage.setItem(getStorageKey(card), 'true');
+    });
+
+    // Duplo clique: volta ao formato original e remove do localStorage
+    $(document).on('dblclick', '.cfg-card', function () {
+        var card = $(this);
+
+        card.removeClass('cfg-card-selecionado');
+        localStorage.removeItem(getStorageKey(card));
     });
 
 });
