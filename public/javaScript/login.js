bd453bb97870cb382d119f4ba5138ecb890a7b6c
João <joaolucassilvasimoes123@gmail.com>
2026-08-25 15:10:46 -0300

    Refazendo  afunction que valida o login

--- public/javaScript/login.js
+++ public/javaScript/login.js
@@ -1,38 +1,46 @@
 $(document).ready(function () {
 
-    $("#formCreateAccount").submit(function (evento) {
+    $("#formLogin").submit(function (evento) {
 
         evento.preventDefault();
 
-        let username = $("#username").val().trim();
-        let email = $("#email").val().trim();
-        let password = $("#password").val().trim();
-        let confirmPassword = $("#confirm-password").val().trim();
+        let username = $("#inputUsername").val().trim();
+        let password = $("#inputPassword").val().trim();
 
-        if (username === "" || email === "" || password === "" || confirmPassword === "") {
-            alert("Preencha todos os campos.");
+        $("#inputUsername").removeClass("is-valid is-invalid");
+        $("#inputPassword").removeClass("is-valid is-invalid");
+
+        let valido = true;
+
+        if (username === "") {
+
+            $("#inputUsername").addClass("is-invalid");
+
+            valido = false;
+
+        } else {
+
+            $("#inputUsername").addClass("is-valid");
+
+        }
+
+        if (password === "") {
+
+            $("#inputPassword").addClass("is-invalid");
+
+            valido = false;
+
+        } else {
+
+            $("#inputPassword").addClass("is-valid");
+
+        }
+
+        if (!valido) {
             return;
         }
 
-        if (password.length < 6) {
-            alert("A senha deve ter pelo menos 6 caracteres.");
-            return;
-        }
-
-        if (password !== confirmPassword) {
-            alert("As senhas não são iguais.");
-            return;
-        }
-
-        var novoUsuario = {
-            username: username,
-            email: email,
-            password: password
-        };
-
-        localStorage.setItem("usuario-logado", JSON.stringify(novoUsuario));
-
-        window.location.href = "home.html";
+        window.location.href = "public/html/home.html";
 
     });
 
