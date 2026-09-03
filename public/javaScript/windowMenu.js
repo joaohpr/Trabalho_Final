944f62c20141dd6d738cc6a1d1c5233c9c54323b
João <joaolucassilvasimoes123@gmail.com>
2026-09-03 18:02:07 -0300

    Novo Menu's window

--- public/javaScript/windowMenu.js
+++ public/javaScript/windowMenu.js
@@ -2,54 +2,41 @@ $(document).ready(function () {
 
     var modal = $('#modal-window');
 
-
     $('#openSettings').click(function () {
-
         abrirModal(modal);
-
     });
 
-
+    // Fechar modal
     modal.on("click", "#backButton", function () {
-
         fecharModal(modal);
-
     });
 
-
-    modal.on('click', '.cfg-card', function () {
+    // Selecionar/deselecionar cards
+    modal.on("click", ".cfg-card", function () {
 
         var card = $(this);
 
-        card.toggleClass('cfg-card-selected');
-
+        card.toggleClass("cfg-card-selected");
     });
 
-
+    // Abrir modal
     function abrirModal(modal) {
 
         modal.load(
             "../html/windowMenu.html #container-main",
-
             function (response, status) {
 
                 if (status === "error") {
-
                     console.error("Erro ao carregar o windowMenu.");
-
                     return;
-
                 }
 
                 modal.css("display", "flex");
-
             }
-
         );
-
     }
 
-
+    // Fechar modal
     function fecharModal(modal) {
 
         modal.css("display", "none").empty();
